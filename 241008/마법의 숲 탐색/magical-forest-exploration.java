40669135386878484bb10ea39cc0d532584b3336
임하은 <73926352+kodms08@users.noreply.github.com>
2024-10-08 03:24:17 +0900

    Title: 마법의 숲 탐색, Time: 198ms, Memory: 14MB, Status: Passed - CodeTree

--- 241008/마법의 숲 탐색/magical-forest-exploration.java
+++ 241008/마법의 숲 탐색/magical-forest-exploration.java
@@ -1,5 +1,5 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
 	static int[][] map;
@@ -8,6 +8,8 @@ public class Main {
 	
 	static int[] dx = {-1, 0, 1, 0}; //북 동 남 서   
 	static int[] dy = {0, 1, 0, -1};
+	static int[][] exit;
+	
 	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
@@ -20,13 +22,14 @@ public class Main {
 		
 		map = new int[R+3][C];
 		low = new int[K];
+		exit = new int[K][2];
 		for(int i=1; i<=K; i++) {
 			st = new StringTokenizer(br.readLine());
 			int c = Integer.parseInt(st.nextToken())-1;
 			int d = Integer.parseInt(st.nextToken());
 			
 			int[] xyd = deep(i, 0, c, d);
-			if(xyd[0]<3) {
+			if(xyd[0]<=3) {
 				map = new int[R+3][C];
 				continue;
 			}
@@ -37,19 +40,12 @@ public class Main {
 				}
 			}
 			low[i-1] = xyd[0]+1-2;
-			int[] e = {xyd[0]+dx[xyd[2]], xyd[1]+dy[xyd[2]]};
-			for(int nd = 0; nd<4; nd++) {
-				if(e[0]+dx[nd]<0||e[0]+dx[nd]>=R+C||
-						e[1]+dy[nd]<0||e[1]+dy[nd]>=C) continue;
-				int n = map[e[0]+dx[nd]][e[1]+dy[nd]];
-				
-				if(n!=0) low[i-1]= Integer.max(low[i-1],low[n-1]);
-			}
+			exit[i-1] = new int[] {xyd[0]+dx[xyd[2]], xyd[1]+dy[xyd[2]]};
+			findMax(i, exit[i-1][0], exit[i-1][1], new boolean[K]);
+			answer+=low[findMax(i, exit[i-1][0], exit[i-1][1], new boolean[K])-1];
 		}
 		
-		for(int l: low) {
-			answer+=l;
-		}
+
 		System.out.println(answer);
 				
 	}
@@ -59,7 +55,7 @@ public class Main {
 		if(south(x,y)) { //하
 			xyd = deep(i,x+1,y,d);
 		}else if(west(x, y)) { //좌
-			xyd = deep(i,x+1,y-1,(d-1)%4);
+			xyd = deep(i,x+1,y-1,(d+3)%4);
 		}else if(east(x, y)) { //우
 			xyd = deep(i,x+1,y+1,(d+1)%4);
 		}
@@ -79,5 +75,18 @@ public class Main {
 		if(y+2<C && x+2<R+3 && map[x][y+2]==0 && map[x+1][y+1]==0
 				&& map[x+1][y+2]==0 && map[x+2][y+1]==0) return true;
 		return false;
+	}
+	static int findMax(int i, int x, int y, boolean[] visit) { 
+		visit[i-1] = true;
+		for(int nd = 0; nd<4; nd++) {
+			if(x+dx[nd]<0||x+dx[nd]>=R+3||y+dy[nd]<0||y+dy[nd]>=C) continue;
+			int n = map[x+dx[nd]][y+dy[nd]];
+			if(n!=0 && !visit[n-1]) {
+				low[i-1] = Integer.max(low[i-1],low[findMax(n, exit[n-1][0], exit[n-1][1], visit)-1]); 
+			}
+			
+		}
+		return i;
+		
 	}
 }
